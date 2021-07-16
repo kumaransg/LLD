65a3cd36bc667dccd7e583b3f1a6ef2ad022ff18
gowtkum <gowtkum@amazon.com>
2021-07-16 08:39:20 +0530

    test

---
+++ LLD Solutions/Bowling-Alley-Machine-Coding-Flipkart/BowlingAlley/src/main/java/Round.java
@@ -0,0 +1,22 @@
+public class Round {
+    private int chance1 = 0;
+
+    public int getChance1() {
+        return chance1;
+    }
+
+    public void setChance1(int chance1) {
+        this.chance1 = chance1;
+    }
+
+    private int chance2 = 0;
+
+    public int getChance2() {
+        return chance2;
+    }
+
+    public void setChance2(int chance2) {
+        this.chance2 = chance2;
+    }
+}
+
